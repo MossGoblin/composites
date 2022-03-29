edff478428f8942d8e099d4e056c905b4f16ae30
Angel Slavchev <aslavchev@roiti.com>
2022-03-29 11:01:59 +0300

    Add Processor base and config.ini

--- utils.py
+++ utils.py
@@ -98,3 +98,15 @@ class ToolBox():
         for count in range(total_count):
             primes.append(next(prime_generator))
         return primes
+
+class Processor():
+
+    def __init__(self, config_file) -> None:
+        self.tb = ToolBox()
+        if config_file == None:
+            config_file = 'config.ini'
+        pass
+
+    def run(self):
+        pass
+
